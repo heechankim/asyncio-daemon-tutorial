797927fd89f60664f94ec7599ec3b824a25d174c
heechankim <heechan.dev@gmail.com>
2022-01-28 18:21:49 +0900

    Feat: Http monitoring provider and check example.com

---
+++ monitoringdaemon/__main__.py
@@ -0,0 +1,19 @@
+""" Main module. """
+
+from dependency_injector.wiring import Provide, inject
+
+from .dispatcher import Dispatcher
+from .containers import Container
+
+
+@inject
+def main(dispatcher: Dispatcher = Provide[Container.dispatcher]) -> None:
+    dispatcher.run()
+
+
+if __name__ == "__main__":
+    container = Container()
+    container.init_resources()
+    container.wire(modules=[__name__])
+
+    main()
---
+++ monitoringdaemon/containers.py
@@ -0,0 +1,35 @@
+""" Containers module. """
+
+import logging
+import sys
+
+from dependency_injector import containers, providers
+
+from . import http, monitors, dispatcher
+
+
+class Container(containers.DeclarativeContainer):
+
+    config = providers.Configuration(yaml_files=["config.yml"])
+
+    logging = providers.Resource(
+        logging.basicConfig,
+        stream=sys.stdout,
+        level=config.log.level,
+        format=config.log.format,
+    )
+
+    http_client = providers.Factory(http.HttpClient)
+
+    example_monitor = providers.Factory(
+        monitors.HttpMonitor,
+        http_client=http_client,
+        options=config.monitors.example,
+    )
+
+    dispatcher = providers.Factory(
+        dispatcher.Dispatcher,
+        monitors=providers.List(
+            example_monitor,
+        ),
+    )
